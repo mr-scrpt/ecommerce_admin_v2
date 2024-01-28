3bd6c4613507bd617203dff08ce9b9cdb987a4e3
Дмитрий Лукьянов <mr@MacBook-Pro-Dmitrij.local>
2024-01-28 12:41:59 +0200

    feat: email login method

--- src/entities/Session/nextAuthConfig.tsx
+++ src/entities/Session/nextAuthConfig.tsx
@@ -4,12 +4,33 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { compact } from "lodash-es";
 import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
 
-const { GITHUB_SECRET, GITHUB_ID } = configPrivate;
+const {
+  GITHUB_SECRET,
+  GITHUB_ID,
+  EMAIL_SERVER_HOST,
+  EMAIL_SERVER_PORT,
+  EMAIL_FROM,
+  EMAIL_SERVER_USER,
+  EMAIL_SERVER_PASSWORD,
+} = configPrivate;
 
 export const nextAuthConfig: AuthOptions = {
   adapter: PrismaAdapter(dbClient) as AuthOptions["adapter"],
   providers: compact([
+    EmailProvider({
+      // ...emailToken,
+      server: {
+        host: EMAIL_SERVER_HOST,
+        port: EMAIL_SERVER_PORT,
+        auth: {
+          user: EMAIL_SERVER_USER,
+          pass: EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: EMAIL_FROM,
+    }),
     GITHUB_ID &&
       GITHUB_SECRET &&
       GithubProvider({
