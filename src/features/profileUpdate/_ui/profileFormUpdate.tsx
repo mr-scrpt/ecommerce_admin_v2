38960f4b3a47c6064124b7eebfad200a8ddfd251
Дмитрий Лукьянов <mr@MacBook-Pro-Dmitrij.local>
2024-02-10 15:14:32 +0200

    refactor: delete ws emit fuction

--- src/features/profileUpdate/_ui/profileFormUpdate.tsx
+++ src/features/profileUpdate/_ui/profileFormUpdate.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useProfileQuery } from "@/entities/user/_query/profile.query";
-import { useListenProfileUpdate } from "@/entities/user/_vm/event/useListenProfileUpdate";
 import { ProfileForm, profileFormSchema } from "@/entities/user/profile";
 import { Spinner } from "@/shared/ui/icons/spinner";
 import { cn } from "@/shared/ui/utils";
@@ -8,9 +7,6 @@ import { useRouter } from "next/navigation";
 import { FC, HTMLAttributes } from "react";
 import { z } from "zod";
 import { useProfileUpdate } from "../_vm/useProfileUpdate";
-import { socketClient } from "@/shared/config/socket";
-import { WSEventEnum } from "@/shared/type/websokcetEvent.enum";
-import { useEmitUserCreate } from "@/entities/user/_vm/event/useEmitUserCreate";
 
 interface ProfileFormProps extends HTMLAttributes<HTMLDivElement> {
   userId: string;
@@ -26,10 +22,7 @@ export const ProfileFormUpdate: FC<ProfileFormProps> = (props) => {
   const { isPending, data } = useProfileQuery(userId);
   const router = useRouter();
 
-  // useListenProfileUpdate();
-
   const { update, isPending: isPendingUpdate } = useProfileUpdate();
-  // const { userCreateEvent } = useEmitUserCreate();
 
   if (isPending) {
     return <Spinner aria-label="Loading profile..." />;
@@ -48,12 +41,6 @@ export const ProfileFormUpdate: FC<ProfileFormProps> = (props) => {
       router.push(callbackUrl);
     }
   };
-
-  // const socket = socketClient("");
-  // socket.connect();
-  // console.log("output_log: on create user =>>>", socket);
-  // socket.emit(WSEventEnum.USER_CREATE);
-  // socket.disconnect();
 
   return (
     <div className={cn(className, "w-full")}>
