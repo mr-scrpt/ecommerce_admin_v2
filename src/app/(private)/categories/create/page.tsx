27c216a6dabddd171a891828769c2e97aa188708
Дмитрий Лукьянов <mr@MacBook-Pro-Dmitrij.local>
2024-02-13 12:51:41 +0200

    refactor: use title page component

--- src/app/(private)/categories/create/page.tsx
+++ src/app/(private)/categories/create/page.tsx
@@ -1,4 +1,5 @@
 import { RoutePathEnum } from "@/shared/config/routing.config";
+import { TitlePage } from "@/shared/ui/titlePage";
 import { CategoryCreate } from "@/widgets/categoryCreate";
 import { FC, HTMLAttributes } from "react";
 
@@ -7,9 +8,8 @@ interface PageProps extends HTMLAttributes<HTMLDivElement> {}
 const CategoryCreatePage: FC<PageProps> = (props) => {
   return (
     <main className="flex min-h-screen flex-col  p-8">
-      <div className="flex justify-between w-full">
-        <h1 className="text-3xl mb-2">Create new category</h1>
-      </div>
+      <TitlePage title="Categories create" />
+
       <CategoryCreate callbackUrl={RoutePathEnum.CATEGORIES} />
     </main>
   );
--- src/app/(private)/categories/update/[slug]/page.tsx
+++ src/app/(private)/categories/update/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { RoutePathEnum } from "@/shared/config/routing.config";
+import { TitlePage } from "@/shared/ui/titlePage";
 import { CategoryUpdate } from "@/widgets/categoryUpdate";
 import { FC, HTMLAttributes } from "react";
 
@@ -12,9 +13,7 @@ const CategoryUpdatePage: FC<PageProps> = (props) => {
   } = props;
   return (
     <main className="flex min-h-screen flex-col  p-8">
-      <div className="flex justify-between w-full">
-        <h1 className="text-3xl mb-2">Create new category</h1>
-      </div>
+      <TitlePage title="Categories update" />
       <CategoryUpdate callbackUrl={RoutePathEnum.CATEGORIES} slug={slug} />
     </main>
   );
