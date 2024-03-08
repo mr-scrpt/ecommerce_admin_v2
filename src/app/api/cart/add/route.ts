28ac7bc6fbd1c8db628f4eb87a32ca3ec8ef777b
mr <mr.scrpt@gmail.com>
2024-03-08 11:42:33 +0200

    refactor: cart add endpoint

--- src/app/api/cart/add/route.ts
+++ src/app/api/cart/add/route.ts
@@ -1,57 +1,28 @@
-import { categorySchema } from "@/entities/category";
-import { getCategoryListAction } from "@/entities/category/server";
-import { getAppSessionStrictServer } from "@/entities/user/user.server";
+import { cartRelationSchema } from "@/entities/cart/_domain/cart.schema";
 import { cartAddProductAction } from "@/features/cartAdd/_action/cartAddProduct.action";
-import { addProductCartUseCase } from "@/features/cartAdd/server";
 import { NextResponse } from "next/server";
 import { z } from "zod";
-
-// interface IMetaCart {
-//   params: {
-//     userId: string;
-//   };
-// }
-
-interface IBodyCart {
-  productId: string;
-}
-
-const metaSchema = z.object({
-  userId: z.string(),
-});
 
 const reqSchema = z.object({
   productId: z.string(),
 });
 
 const resultSchema = z.object({
-  data: z.array(categorySchema),
+  data: cartRelationSchema,
 });
 
-export const PUT = async (
-  req: Request,
-  // meta: IMetaCart,
-): Promise<NextResponse<any>> => {
+export const PUT = async (req: Request): Promise<NextResponse<any>> => {
   try {
     const { productId } = reqSchema.parse(await req.json());
 
-    const cart = await cartAddProductAction({
+    const { cart } = await cartAddProductAction({
       data: {
         productId,
         quantity: 1,
       },
     });
-    console.log("output_log: cart in api =>>>", cart);
 
-    // const cart = await addProductCartUseCase.exec({
-    //   dataToAddProduct: {
-    //     productId,
-    //     quantity: 1,
-    //   },
-    //   session,
-    // });
-
-    return NextResponse.json(cart);
+    return NextResponse.json(resultSchema.parse({ data: cart }));
   } catch (e) {
     return NextResponse.json(e);
   }
