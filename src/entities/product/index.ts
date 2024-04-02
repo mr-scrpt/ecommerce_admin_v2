c9613b6259e5ba8c4b0a935fdd58e16ad0c7caef
mr <mr.scrpt@gmail.com>
2024-04-02 13:03:15 +0300

    fix: import type

--- src/entities/product/index.ts
+++ src/entities/product/index.ts
@@ -23,4 +23,5 @@ export type {
   ProductRelation,
   ProductToCreate,
   ProductToUpdate,
+  ProductToSelect,
 } from "./_domain/types";
--- src/features/orderCreate/_vm/useOrderCreate.tsx
+++ src/features/orderCreate/_vm/useOrderCreate.tsx
@@ -1,4 +1,3 @@
-import { OrderGroup } from "@/entities/order/_domain/order.types";
 import { useOrderCreateMutation } from "../_mutation/useCreate.mutation";
 
 export const useOrderCreate = (orderId: string) => {
@@ -11,7 +10,6 @@ export const useOrderCreate = (orderId: string) => {
   return {
     orderCreate: (data: OrderGroup) =>
       orderCreate({
-        orderId,
         order: data.order,
         payment: data.payment,
       }),
