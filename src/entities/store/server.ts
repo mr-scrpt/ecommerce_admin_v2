ab729991640c73c0a2d0f38ddb2098b6d9296710
mr <mr.scrpt@gmail.com>
2024-04-24 14:52:01 +0300

    fix: import

--- src/entities/store/server.ts
+++ src/entities/store/server.ts
@@ -1,6 +1,6 @@
-export { CategoryRepository } from "./_repository/store.repo";
-export { createCategoryAbility } from "./_domain/store.ability";
-export { categorySchema } from "./_domain/store.schema";
+export { StoreRepository } from "./_repository/store.repo";
+export { createStoreAbility } from "./_domain/store.ability";
+export { storeSchema } from "./_domain/store.schema";
 
-export { getCategoryListAction } from "./_action/getStoreList.action";
-export { getCategoryAction } from "./_action/getStore.action";
+export { getStoreListAction } from "./_action/getStoreList.action";
+export { getStoreAction } from "./_action/getStore.action";
