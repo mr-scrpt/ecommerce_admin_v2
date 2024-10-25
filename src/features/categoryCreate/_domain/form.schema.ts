14c31660292a53d573f8535b4802eddde0577555
mr.scrpt <mr.scrpt@gmail.com>
2024-10-25 15:21:28 +0300

    fix: schema option type

--- src/features/categoryCreate/_domain/form.schema.ts
+++ src/features/categoryCreate/_domain/form.schema.ts
@@ -1,10 +1,10 @@
 import { categoryFormDefaultSchema } from "@/entities/category";
-import { propertyItemDefaultSelectOptionSchema } from "@/kernel/domain/property/form.schema";
+import { propertyDefaultSelectOptionSchema } from "@/kernel/domain/property/form.schema";
 import { z } from "zod";
 
 export const categoryCreateFormSchema = categoryFormDefaultSchema
   .extend({
-    propertyList: z.array(propertyItemDefaultSelectOptionSchema),
+    propertyList: z.array(propertyDefaultSelectOptionSchema),
   })
   .omit({
     categoryList: true,
